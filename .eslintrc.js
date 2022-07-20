57fc1b2a737329d075d167b6a33b690154fc9736
feel0321 <dishate@naver.com>
2022-07-20 16:50:32 +0900

    chore: .vscode 파일 추가. eslint에 prettier 추가. eslint rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,42 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "prettier/prettier": "off",
+    "no-nested-ternary": "off",
+    "no-unused-vars": "warn",
+    "no-console": "warn",
+    "import/prefer-default-export": "warn",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
   },
 };
